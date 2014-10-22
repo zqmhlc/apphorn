4daa4a69c521c283b62a6e33f8500d789cff74b9
zqmhlc <zqmhlcv@163.com>
2014-10-22 14:06:30 +0800

    test

--- app/app/js/index.js
+++ app/app/js/index.js
@@ -18,25 +18,26 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        setDeviceInfo();
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,3 +48,14 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+function setDeviceInfo() {
+    var element = document.getElementById('deviceProperties');
+    element.innerHTML = 'Device Name: ' + device.name + '<br />' +
+                            'Device Cordova: ' + device.cordova + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: ' + device.uuid + '<br />' +
+                            'Device Model: ' + device.model + '<br />' +
+                            'Device Version: ' + device.version + '<br />';
+}
